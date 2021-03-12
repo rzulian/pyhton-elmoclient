a1da9becb3aaeee8496554b460e4b1fdbb4eecca
Riccardo Zulian <riccardo.zulian@gmail.com>
2021-03-12 10:46:22 +0100

    initial code commit

---
+++ elmoclient/__init__.py
@@ -0,0 +1,273 @@
+import socket
+import binascii
+import logging
+import time
+import threading
+import queue
+from .elmoprocessor import *
+
+_LOGGER = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] (%(threadName)-10s) %(message)s")
+SIGTYPES =  ["ingresso", "uscita", "settore", "uscita_dedicata", "memoria_uscita_dedicata", "anomalia", "settore_inseribile"]
+
+class PollThread(threading.Thread):
+    """Process poll update requests."""
+
+    def __init__(self, elmo):
+        """Set up the Elmo outgoing packet processing thread."""
+        self._stop_event = threading.Event()
+        self.elmo = elmo
+        threading.Thread.__init__(self, name="Send")
+
+    def run(self):
+        """Start the Elmo outgoing packet processing thread."""
+        _LOGGER.debug("polling thread start")
+
+        while not self._stop_event.is_set():
+
+            # process the queue
+            while self.elmo.connected is True and not self.elmo.tx_queue.empty():
+                command, tx = self.elmo.tx_queue.get()
+                if self.elmo.restart_connection is False:
+                    _LOGGER.debug(
+                        f"TX:{command} <{str(binascii.hexlify(tx), 'ascii')}>")
+                    try:
+                        self.elmo.socket.sendall(tx)
+                    except socket.error:
+                        with self.elmo.restart_lock:
+                            self.elmo.restart_connection = True
+                    else:
+                        data = self.elmo.socket.recv(4096)
+                        print('Received: %r' % binascii.hexlify(data))
+                        if (command=='lettura_inseribili'):
+                            self.elmo.parse_settori_inseribili(data)
+            
+            time.sleep(0.2)
+            # request a status update
+            if (self.elmo.connected is True
+                and self.elmo.restart_connection is False
+                    and self.elmo.polling_enabled is True):
+                try:
+                    self.elmo.socket.send(bytes.fromhex('02010800003f004803'))
+                except socket.error:
+                    with self.elmo.restart_lock:
+                        self.elmo.restart_connection = True
+                data = self.elmo.socket.recv(4096)                  
+                self.elmo.parse_update(data) 
+                # print('Received: %r' % binascii.hexlify(data))
+
+        _LOGGER.debug("polling thread stop")
+
+    def join(self, timeout=None):
+        """Stop the Elmo outgoing packet processing thread."""
+        self._stop_event.set()
+        _LOGGER.debug("set() in polling")
+        threading.Thread.join(self, timeout)
+
+
+class ConnectionThread(threading.Thread):
+    """Manage the socket connection to the control processor."""
+
+    def __init__(self, elmo):
+        """Set up the socket management thread."""
+        self._stop_event = threading.Event()
+        self.elmo = elmo
+        threading.Thread.__init__(self, name="Connection")
+
+    def run(self):
+        """Start the socket management thread."""
+        _LOGGER.debug("connection thread start")
+
+        warning_posted = False
+
+        while not self._stop_event.is_set():
+            try:
+                self.elmo.socket = socket.socket(
+                    socket.AF_INET, socket.SOCK_STREAM)
+                self.elmo.socket.settimeout(self.elmo.timeout)
+                self.elmo.socket.connect((self.elmo.host, self.elmo.port))
+            except socket.error:
+                self.elmo.socket.close()
+                if warning_posted is False:
+                    _LOGGER.debug(
+                        f"attempting to connect to {self.elmo.host}:{self.elmo.port}, "
+                        "no success yet"
+                    )
+                    warning_posted = True
+                if not self._stop_event.is_set():
+                    time.sleep(1)
+            else:
+                warning_posted = False
+                _LOGGER.debug(f"connected to {self.elmo.host}:{self.elmo.port}")
+                if not self.elmo.restart_connection:
+                    self.elmo.poll_thread.start()
+                self.elmo.restart_connection = False
+                while (
+                    not self._stop_event.is_set()
+                    and self.elmo.restart_connection is False
+                ):
+                    time.sleep(1)
+                if not self._stop_event.is_set():
+                    self.elmo.connected = False
+                    self.elmo.socket.close()
+                    _LOGGER.debug(
+                        f"lost connection to {self.elmo.host}:{self.elmo.port}")
+                else:
+                    # close all threads
+                    self.elmo.poll_thread.join()
+
+        self.elmo.socket.close()
+
+        _LOGGER.debug("connection thread stop")
+
+    def join(self, timeout=None):
+        """Stop the socket management thread."""
+        self._stop_event.set()
+        _LOGGER.debug("set() in connection")
+        threading.Thread.join(self, timeout)
+
+
+class ElmoClient:
+
+    def __init__(self, host, port=10001, timeout=2, num_ingressi=32, num_uscite=32):
+        """ Initialize ElmoClient object """
+        self.host = host
+        self.port = port
+        self.socket = None
+        self.timeout = timeout
+        self.polling_enabled = False
+ 
+        self._client = None
+        self._prev_status = None
+        self.connected = False
+        self.restart_lock = threading.Lock()
+        self.restart_connection = False
+
+        self.connection_thread = ConnectionThread(self)
+        self.poll_thread = PollThread(self)
+
+        self.tx_queue = queue.Queue()
+
+        self.join_lock = threading.Lock()
+        self._status = {
+            "ingresso": {},
+            "uscita": {},
+            "settore": {},
+            "uscita_dedicata": {},
+            "memoria_uscita_dedicata": {},
+            "anomalia": {},
+            "settore_inseribile": {},
+        }
+
+
+    def start(self):
+        """Start the Elmo client instance."""
+        if self.connection_thread.is_alive():
+            _LOGGER.error("start() called while already running")
+        else:
+            _LOGGER.debug("connection thread start requested")
+            self.connection_thread.start()
+            self.connected = True
+
+    def stop(self):
+        """Stop the Elmo client instance."""
+        if not self.connection_thread.is_alive():
+            _LOGGER.error("stop() poll thread called while already stopped")
+        else:
+            _LOGGER.debug("connection thread stop requested")
+            self.connection_thread.join()
+
+    def inserisci_settore(self, num_settore):
+        cmd = cmd_inserisci_settore(num_settore)
+        cmd = rq_cmd(cmd)
+        cmd = parse_to_send(cmd)
+        self.tx_queue.put(('ins_settore', cmd))
+
+    def disinserisci_settore(self, num_settore):
+        cmd = cmd_disinserisci_settore(num_settore)
+        cmd = rq_cmd(cmd)
+        cmd = parse_to_send(cmd)
+        self.tx_queue.put(('disins_settore', cmd))
+
+    def richiedi_lettura_settori_inseribili(self):
+        cmd = cmd_lettura_settori_inseribili()
+        cmd = rq_cmd(cmd)
+        cmd = parse_to_send(cmd)
+        self.tx_queue.put(('lettura_inseribili', cmd))
+
+    def richiedi_lettura_stato_ingressi(self):
+        cmd = cmd_lettura_stato_ingressi()
+        cmd = rq_cmd(cmd)
+        cmd = parse_to_send(cmd)
+        self.tx_queue.put(('lettura_ingressi', cmd))
+
+    def get(self, sigtype, pos):
+        """Get the current value of a pos."""
+        if (sigtype not in SIGTYPES):
+            raise ValueError(f"get(): '{sigtype}' is not a valid signal sigtype")
+
+        with self.join_lock:
+            try:
+                value = self._status[sigtype][pos][0]
+            except KeyError:
+                value = 0
+        return value
+    
+    def subscribe(self, sigtype, pos, callback):
+        """Subscribe to join change events by specifying callback functions."""
+        if (sigtype not in SIGTYPES):
+            raise ValueError(f"subscribe(): '{sigtype}' is not a valid signal sigtype")
+
+        with self.join_lock:
+            if pos not in self._status[sigtype]:
+                self._status[sigtype][pos] = [
+                    0,
+                ]
+            self._status[sigtype][pos].append(callback)
+
+    def update_signals(self, sigtype, data):
+        for i in range(len(data)):
+            try:
+                pos = i+1
+                value = int(data[i])
+                # updates the value only if changed
+                if self._status[sigtype][pos][0] != value:
+                    self._status[sigtype][pos][0] = value
+                    for callback in self._status[sigtype][pos][1:]:
+                        callback(sigtype[0], pos, value)
+                    _LOGGER.debug(f"  : {sigtype} {pos} = {value}")
+            except KeyError:
+                self._status[sigtype][pos] = [
+                    value,
+                ]
+
+    
+    def parse_update(self, data):
+        """ parse incoming status update only when different from the previous status """
+        if (data == self._prev_status ):    
+            return       
+                    
+        # print('to be parsed: %r' % binascii.hexlify(data)) 
+        decode = recive(data)
+        # print(f"TX: <{str(binascii.hexlify(decode), 'ascii')}>")
+        # riduco stringa scartando Lmsg + Flag +Ind(msb) + Ind(lsb) 
+        (self._ingressi, self._memoria_ingressi, self._uscite, self._settori, self._settori_max_sicurezza, self._anomalia, self._uscita_dedicata, self._memoria_uscita_dedicata) = read_stato_allineamento_ridotto(decode[4:])
+        self.update_signals('ingresso', self._ingressi)
+        self.update_signals('uscita', self._uscite)
+        self.update_signals('settore', self._settori)
+        self.update_signals('anomalia', self._anomalia)
+        self.update_signals('uscita_dedicata', self._uscita_dedicata)
+        self.update_signals('memoria_uscita_dedicata', self._memoria_uscita_dedicata)
+        self._prev_status = data
+        # aggiorna lo stato degli inseribili visto che è cambiato qualcosa
+        self.richiedi_lettura_settori_inseribili()
+
+    def parse_settori_inseribili(self, data):
+        decode = recive(data)
+        self._settori_inseribili = read_settori_inseribili(decode[4:])
+        self.update_signals('settore_inseribile', self._settori_inseribili)
+
+    def parse_stato_ingressi(self, data):
+        decode = recive(data)
+        self._ingressi = read_stato_ingressi(decode[4:])
+        self.update_signals('ingresso', self._ingressi)
---
+++ elmoclient/elmoprocessor.py
@@ -0,0 +1,217 @@
+import binascii
+from typing import ByteString
+
+STX = 0x02 # Inizio ricezione
+ETX = 0x03 # fine ricezione
+DLE = 0x10 # byte stuffing
+
+# Flag direzione messaggio (da)
+FLAG_CRESTRON = 0x08 
+FLAG_ELMO = 0x28 
+
+# Response
+ACK = 0x06	# Ok
+NAK = 0x15	# error
+ENQ	= 0x05	# comand not available
+BEL = 0x07  # access denied
+
+# Costanti Controllo Remoto
+CENTRALE	=	0x01
+GRUPPO		=	0x09
+INGRESSO	=	0x0A
+UTENTE		=	0x0B
+USCITA		=	0x0C
+
+# Comando
+INSERIMENTO	= 0x01		# inserimento Attivazione Abilitazione
+DISINSERIMENTO = 0x02		# Disinsermento Disattivazione Esclusione
+INSERIMENTOMAXSICUREZZA	= 0x18		
+DISINSERIMENTOMAXSICUREZZA = 0x19
+
+STATOCONNESSIONE   		= 0x21		#"!"
+STATOINGRESSI			= 0x22		#"""
+RICHIESTAVERSIONE		= 0x24		#"$"
+RESETPUNTATOREEVENTI	= 0x26		#"&"
+LEGGINUOVIEVENTI		= 0x2B		#"+"
+STATOINGRESSIESC_ATT	= 0x2C		#","
+LETTURAMEMORIA			= 0x3C		#"<"  Una volta ottenute le informazioni dell’indirizzo di partenza
+														# dal STATUSINFO per leggere la memoria per la dimensione desiderata
+LEGGISTRINGHE			= 0x3D		#"="  seguito da 3 byte organizzati come segue:
+														#[ClasseElemento][NumeroElementoH][NumeroElementoL]
+														#al quale la centrale risponde con una stringa (non terminata a 0).
+ALLINEAMENTORIDOTTO		= 0x3F		#"?"
+ACCESSO_AL_SISTEMA		= 0x41		#"?"
+LETTURAINSERIBILI		= 0x5E		#"^"  consente di conoscere i settori inseribili in modo totale e parziale
+IMPOSTAZIONEDATAORA		= 0x7B		#"{"
+LEGGIORADATA			= 0x7D		#"}"
+STATUSINFO				= 0x7E		#"~"  consente di ottenere lo stato completo della centrale.
+CONTROLLOREMOTO			= 0x2D	
+
+def byte_stuffing(to_convert):
+    stuffed = bytearray()
+    for x in to_convert:
+        if ( x == STX or  x == ETX or x == DLE):
+            stuffed += DLE.to_bytes(1,'big') 
+            x =  x + 0x80
+        stuffed += x.to_bytes(1,'big')
+    return stuffed
+
+def byte_unstuffing(to_convert):
+    unstuffed = bytearray()
+    dle = False
+    for x in to_convert:
+        if (x == DLE): 
+            dle = True
+            continue
+        if dle :
+            x =  x - 0x80
+            dle = False
+
+        unstuffed += x.to_bytes(1,'big')
+    return unstuffed
+
+def crc2(to_crc):
+    crc = 0
+    for x in to_crc:
+        crc += x
+    return crc
+
+def segmento32( comando, classe, elemento):
+    segmento = bytearray()
+    segmento += comando.to_bytes(1,'big')
+    segmento += classe.to_bytes(1,'big')
+    segmento += elemento.to_bytes(2,  byteorder ='big')
+    segmento += (0).to_bytes(16,  byteorder ='big')
+    return segmento
+
+def rq_cmd(data: bytearray ):
+    payload = bytearray()
+    lmsg = len(data)
+    flag = 0x08;
+    payload += lmsg.to_bytes(1,'big')
+    payload += flag.to_bytes(1,'big')
+    payload += (0).to_bytes(2,  byteorder ='big')
+    payload += data
+    return payload
+
+
+def parse_to_send(to_send):
+    
+    comando = to_send[4]
+    lmsg = to_send[0]
+    if ( lmsg == len(to_send) - 4 ):
+        flag = to_send[1]
+        ind = (to_send[2]<<8) + to_send[3] 
+        checksum = crc2(to_send)
+        to_send += checksum.to_bytes(2, 'big')
+        to_send = byte_stuffing(to_send)
+        to_send = b'\x02' + to_send + b'\x03'
+
+    return to_send
+    
+def recive(to_read):
+    data_length = to_read[1];
+    response =  to_read[5];
+    # scarta inizio e fine
+    to_read = to_read[1:len(to_read)-1]
+
+    to_read = byte_unstuffing(to_read)
+    length = len(to_read);
+    crc = (to_read[length-2]<<8) + to_read[length-1] 
+    # toglie crc
+    to_read = to_read[0:length-2]
+    # todo verifica ncrc = crc2(to_read)
+    # print(ncrc, crc)
+
+    # in crestron restituisce il comando che è stato inviato 
+    # probabilmente perchè non c'è il multi treading
+    # FORMATO Lmsg + Flag +Ind(msb) + Ind(lsb) + Stringacmd 
+    # a questo punto dovrebbe data_length + 4 = len(to_read)
+
+    return to_read
+
+
+def encrypt_password(password):
+
+    return password
+
+def cmd_inserisci_settore(settore):
+    cmd = bytearray()
+    cmd += CONTROLLOREMOTO.to_bytes(1,'big')
+    cmd += segmento32(INSERIMENTO, GRUPPO, settore)
+    return cmd
+
+def cmd_disinserisci_settore(settore):
+    cmd = bytearray()
+    cmd += CONTROLLOREMOTO.to_bytes(1,'big')
+    cmd += segmento32(DISINSERIMENTO, GRUPPO, settore)
+    return cmd
+
+def cmd_lettura_settori_inseribili():
+    cmd = LETTURAINSERIBILI.to_bytes(1,'big')
+    return cmd
+
+def cmd_lettura_stato_ingressi():
+    cmd = STATOINGRESSI.to_bytes(1,'big')
+    return cmd
+
+def read_stato_allineamento_ridotto(data):
+    offset = 9
+    num_bytes_ingressi = data[0]
+    num_bytes_memoria_ingressi = data[1]
+    num_bytes_uscite = data[2]
+    num_bytes_uscite_dedicate = data[3]
+    num_bytes_memoria_uscite_dedicate = data[4]
+    num_bytes_settori = data[5]
+    num_bytes_settori_max_sicurezza = data[7]
+
+    blocco_start = offset
+    stato_ingressi = data[ blocco_start: blocco_start + num_bytes_ingressi ]
+    ingressi = ''.join(format(byte, '08b') for byte in stato_ingressi)
+    
+    blocco_start += num_bytes_ingressi
+    stato_memoria_ingressi = data[ blocco_start: blocco_start + num_bytes_memoria_ingressi ]
+    memoria_ingressi = ''.join(format(byte, '08b') for byte in stato_memoria_ingressi)
+
+    blocco_start += num_bytes_memoria_ingressi
+    stato_uscite = data[ blocco_start: blocco_start + num_bytes_uscite ]
+    uscite = ''.join(format(byte, '08b') for byte in stato_uscite)
+
+    # uscite dedicate e memorie dovrebbero essere un solo byte
+    # le posizioni sono quelle dei bit e non bit(pos)
+    # viene invertita ciascuna conversione
+    blocco_start += num_bytes_uscite
+    stato_uscite_dedicate = data[ blocco_start: blocco_start + num_bytes_uscite_dedicate ]
+    uscite_dedicate = ''.join(format(byte, '08b')[::-1] for byte in stato_uscite_dedicate)
+
+
+    blocco_start += num_bytes_uscite_dedicate 
+    stato_memoria_uscite_dedicate = data[ blocco_start: blocco_start + num_bytes_memoria_uscite_dedicate ]
+    memoria_uscite_dedicate = ''.join(format(byte, '08b')[::-1] for byte in stato_memoria_uscite_dedicate)
+
+    blocco_start += num_bytes_memoria_uscite_dedicate
+    stato_settori = data[ blocco_start: blocco_start + num_bytes_settori ]
+    settori = ''.join(format(byte, '08b') for byte in stato_settori)
+
+      
+    blocco_start += num_bytes_settori
+    stato_settori_max_sicurezza = data[ blocco_start: blocco_start + num_bytes_settori_max_sicurezza ]
+    settori_max_sicurezza = ''.join(format(byte, '08b') for byte in stato_settori_max_sicurezza)
+    
+     # le posizioni sono quelle dei bit e non bit(pos)
+    anomalia = format(data[blocco_start + num_bytes_settori_max_sicurezza], '08b')[::-1]
+    return (ingressi, memoria_ingressi, uscite, settori, settori_max_sicurezza, anomalia, uscite_dedicate, memoria_uscite_dedicate)
+
+def read_settori_inseribili(data):
+    num_bytes_settori_inseribili = data[0]
+    
+    stato_settori_inseribili = data[ 2: 2 + num_bytes_settori_inseribili ]
+    settori_inseribili = ''.join(format(byte, '08b') for byte in stato_settori_inseribili)
+    return settori_inseribili
+
+def read_stato_ingressi(data):
+    num_bytes_ingressi= data[0]
+    
+    stato_ingressi= data[ 2: 2 + num_bytes_ingressi ]
+    ingressi = ''.join(format(byte, '08b') for byte in stato_ingressi)
+    return ingressi
